4b979e0f2aaadc9c42127927ad8086ddab344ad0
motomotoyoshi <motomoto.git@gmail.com>
2020-01-16 00:14:20 +0900

    Fix comment

--- make_muliple_array_to_json.php
+++ make_muliple_array_to_json.php
@@ -13,21 +13,21 @@ $array = [];
 foreach($items as $item) {
     preg_match("/item\d{1,3}/", $item, $key);
 
-    // idを生成
+    // key=idを生成
     if(!in_array($key[0], $array_index)) {
         array_push($array_index, $key[0]);
         $array_index_num = array_search($key[0], $array_index);
         array_push($array, array('id' => $key[0]));
     }
 
-    // headを生成
+    // key=key1を生成
     if (preg_match("/key1=.*/", $item)) {
         preg_match("/key1=.*/", $item, $key1);
         $key1 = str_replace("key1=", "", $key1[0]);
         $array[$array_index_num]['key1'] = $key1;
     }
 
-    // bodyを生成
+    // key=key2を生成
     if (preg_match("/key2=.*/", $item)) {
         preg_match("/key2=.*/", $item, $key2);
         $key2 = str_replace("key2=", "", $key2[0]);
